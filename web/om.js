11b2f784d7a84bed1e25390551e2480f5c7ee39e
kakashi <m.ashutoshsoni@gmail.com>
2017-10-22 08:35:21 +0530

    Internship Project

---
+++ web/om.js
@@ -0,0 +1,18 @@
+function live() {
+console.log('hell');
+if(document.getElementById('username').value!="root")
+{
+
+window.location.assign("/userindex.html");
+
+}
+
+else{
+
+
+  window.location.assign("/index.html");
+
+
+  }
+}
+  live();
