9c08f097d09e19230cb35b71587ad5aec29380a9
kakashi <m.ashutoshsoni@gmail.com>
2017-10-23 15:32:13 +0530

    Updateing root password

--- web/om.js
+++ web/om.js
@@ -1,13 +1,14 @@
 function live() {
 console.log('hell');
-if(document.getElementById('username').value!="root")
+
+if(document.getElementById('username').value!="root"  )
 {
 
 window.location.assign("/userindex.html");
 
 }
 
-else{
+else if (document.getElementById('password').value=="root@123" ){
 
 
   window.location.assign("/index.html");
